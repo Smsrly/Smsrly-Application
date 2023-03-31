c6aba8f4e84232e06df76be7a8ac70168dec867f
AhmedEbeid4 <ebeidahmed2@gmail.com>
2023-03-31 11:24:19 +0200

    flutter toast added

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,3 +1,11 @@
-class Utils{
+import 'package:fluttertoast/fluttertoast.dart';
 
+class Utils {
+  static getToast(String msg, int i) {
+    Fluttertoast.showToast(
+      msg: msg ,
+      toastLength: i == 0 ? Toast.LENGTH_SHORT : Toast.LENGTH_LONG,
+      gravity: ToastGravity.BOTTOM,
+    );
+  }
 }
